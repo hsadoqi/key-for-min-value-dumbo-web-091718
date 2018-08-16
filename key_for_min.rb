e314eae8aba469cb7b6d51b62bb212f61450659b
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-16 04:10:59 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,10 @@ def key_for_min_value(name_hash)
   if name_hash.size == 0 nil 
   else 
     name_hash.each do |key, value|
-      
+      if min_value == nil || min_value > value 
+        min_value = value
+        min_key = key
+      end 
+    end
+    min_value
 end
